0f19b791c76e3ab8fdbaf583a149c1691bb380e4
Johan Sylvan <gtc124@gmail.com>
2018-10-23 13:42:36 +0200

    Add api endpoint

--- api/api.js
+++ api/api.js
@@ -1,7 +1,12 @@
 const fs = require("fs")
 
 const sql = require("sqlite3").verbose()
-const knex = require("knex")
+const knex = require("knex")({
+  client: "sqlite3",
+  connection: {
+    filename: "api.db"
+  }
+})
 
 setupAPIDatabase = () => {
   let db = new sql.Database(
@@ -11,19 +16,48 @@ setupAPIDatabase = () => {
       if (err) {
         console.log("There was an error opening or creating the database.")
         console.log(err)
-        db.close();
-      } else console.log("Database opened.")
+        db.close()
+        return
+      }
     }
   )
 
   db.run(fs.readFileSync("init.sql", "utf-8"))
 
   db.close(err => {
-    if(err) console.log("Failed to close database")
-    else console.log("Closed conenction to database")
+    if (err) console.log("Failed to close database")
   })
 }
 
+function getRoutes(callback) {
+  let db = new sql.Database("api.db", sql.OPEN_READONLY)
+  let data = []
+  db.each(
+    "SELECT route, action FROM routes WHERE method='GET'",
+    (err, row) => {
+      if (!err) data.push(row)
+      else data = "Not wah"
+    },
+    () => {
+      db.close()
+      callback(data)
+    }
+  )
+}
 
+function registerRoute(route, action, callback, method = "GET") {
+  let db = new sql.Database("api.db", sql.OPEN_READWRITE)
+  db.run(
+    `INSERT INTO routes (route, method, action) VALUES ('${route}', '${method}', '${action}')`,
+    () => {
+      return
+    },
+    err => {
+      callback(err ? false : true)
+    }
+  )
+}
 
-module.exports.setup = setupAPIDatabase;
+module.exports.setup = setupAPIDatabase
+module.exports.routes = getRoutes
+module.exports.register = registerRoute
--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const express = require("express");
+const express = require("express")
 
-const app = express();
+const app = express()
 
 const api = require("./api/api")
-api.setup();
 
+app.listen(3000)
 
-app.get("/", (req, res) => {res.send("API Builder")});
+api.setup()
+api.routes(data => {
+  data.forEach(route => {
+    app.get(`/api/${route.route}`, (req, res) => {
+      res.send(route.route)
+    })
+  })
+})
 
-app.listen(3000);
+app.get("/", (req, res) => {
+  res.send("<h1>API Builder</h1><p>Documentation to come</p>")
+})
+
+app.get("/build/register", (req, res) => {
+  api.register(req.query.route, req.query.action, registered => {
+    res.send((registered ? "Saved route" : "Failed to save route"))
+  })
+})

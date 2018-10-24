3f6d90ce8d47fa4879e9494c90dddeb17f44623a
Johan Sylvan <gtc124@gmail.com>
2018-10-24 23:28:13 +0200

    Moved opening endpoints to function
    Does not work completely yet

--- index.js
+++ index.js
@@ -4,25 +4,33 @@ const app = express()
 
 const api = require("./api/api")
 
-app.listen(3000)
-
-api.setup()
-api.routes(data => {
-  data.forEach(route => {
-    app.get(`/api/${route.route}`, (req, res) => {
-      api.return(JSON.parse(route.action), data => {
-        res.send(data)
+function openEndpoints(api) {
+  api.routes(data => {
+    data.forEach(route => {
+      app.get(`/api/${route.route}`, (req, res) => {
+        api.return(JSON.parse(route.action), data => {
+          res.send(data)
+        })
       })
     })
   })
-})
+}
+
+app.listen(3000)
+
+api.setup()
+openEndpoints(api)
+
 
 app.get("/", (req, res) => {
   res.send("<h1>API Builder</h1><p>Documentation to come</p>")
 })
 
 app.get("/build/register", (req, res) => {
+  console.log(api)
   api.register(req.query.database, req.query.route, req.query.action, registered => {
     res.send((registered ? "Saved route" : "Failed to save route"))
+    console.log(api)
+    openEndpoints(api)
   })
 })

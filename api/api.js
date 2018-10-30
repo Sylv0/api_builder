c35eb07eb0ddaa714b925a68bee42534fa3e1d7d
Johan Sylvan <gtc124@gmail.com>
2018-10-30 10:16:25 +0100

    Made things easier when switching between databases

--- api/api.js
+++ api/api.js
@@ -4,7 +4,7 @@ const fs = require("fs")
 
 const sql = require("sqlite3").verbose()
 
-const knex = new require("./sqlite")("api.db")
+const knex = new require("./sqlite")({url: "api.db"})
 let target
 
 const setupAPIDatabase = () => {
@@ -41,7 +41,7 @@ function setTargetDatabase(database) {
   target = null;
   return new Promise((resolve, reject) => {
     getDatabases(database).then(newTarget => {
-      target = require("./sqlite")(newTarget[0].url)
+      target = require("./sqlite")(newTarget[0])
       if(target) resolve()
       else reject()
     })
--- api/sqlite.js
+++ api/sqlite.js
@@ -1,8 +1,8 @@
-module.exports = path => {
+module.exports = target => {
   return require('knex')({
     client: "sqlite3",
     connection: {
-      filename: path
+      filename: target.url
     },
     useNullAsDefault: true
   })
